1d57e1e4adf5864a5151ef9810bf3dea27d93619
Must <nm.ekinci@gmail.com>
2023-09-06 09:13:44 +0200

    starter

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        position: "absolute",
+        bottom: "0",
+        backgroundColor: "#A4D5A6",
+        width: "100%",
+      }}
+    >
+      <p>Developed by</p>
+      <p>copyright</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <NavBar />
+      <Footer />
+    </div>
+  );
+};
+
+export default Dashboard;
