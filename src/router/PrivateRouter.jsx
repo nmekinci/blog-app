f5b6d588cf869ef4f794db78cac1bbcebced522e
Must <nm.ekinci@gmail.com>
2023-09-07 21:52:58 +0200

    creat privaterouter

--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../context/AuthContext'
+import { Navigate, Outlet } from 'react-router-dom'
 
 const PrivateRouter = () => {
-  return (
-    <div>PrivateRouter</div>
-  )
+
+  const {currentuser} = useContext(AuthContext)
+  
+  
+  return currentuser ? <Outlet /> : <Navigate to="/auth" replace/>
 }
 
 export default PrivateRouter
