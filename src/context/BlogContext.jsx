b3fa1222dedc40f9406715b7ac86bdabcb0aa623
Must <nm.ekinci@gmail.com>
2023-09-08 17:03:30 +0200

    prep

---
+++ src/context/BlogContext.jsx
@@ -0,0 +1,11 @@
+// getBlogs = get http://30117.fullstack.clarusway.com/  api/blogs/
+
+// createBlogs = post http://30117.fullstack.clarusway.com/    api/blogs/, {
+//   "title": "string",
+//   "content": "string",
+//   "image": "string",
+//   "category": 0,
+//   "status": "d",
+//   "slug": "string"
+// }
+
