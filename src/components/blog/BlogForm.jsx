99dd9639ccf480eb0a2e15b9dc812875c8337728
Must <nm.ekinci@gmail.com>
2023-09-09 11:11:28 +0200

    creat UpdateModal.jsx BlogForm.jsx

--- src/components/blog/BlogForm.jsx
+++ src/components/blog/BlogForm.jsx
@@ -5,25 +5,40 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { Avatar, Box, IconButton } from "@mui/material";
+import { Avatar, Box, IconButton, Modal } from "@mui/material";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ForumIcon from "@mui/icons-material/Forum";
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
 import { useNavigate } from "react-router-dom";
 import { pink } from "@mui/material/colors";
 import CommentForm from "./CommentForm";
-
+import UpdateModal from "./UpdateModal";
 
 const BlogForm = () => {
-const [toggle, setToggle] = React.useState(false)
+  const [toggle, setToggle] = React.useState(false);
   const navigate = useNavigate();
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
 
   const handleClick = () => {
     navigate("/my-blog");
   };
   const handleToggle = () => {
-setToggle(!toggle)
-  }
+    setToggle(!toggle);
+  };
   return (
     <Box
       sx={{
@@ -80,17 +95,19 @@ setToggle(!toggle)
           </Box>
         </CardContent>
 
-        <Box
-          sx={{ display: "flex", justifyContent: "space-between", padding: 2 }}
-        >
-          <Box sx={{ gap: 2 }}>
+        <Box sx={{ padding: 2 }}>
+          <Box sx={{ gap: 2, textAlign: "center", mb: 2 }}>
             <IconButton aria-label="favorites" sx={{ gap: 1 }}>
               <FavoriteIcon sx={{ mt: 1, width: 18, height: 18 }} />
               <Typography variant="body2" sx={{ mt: 1 }}>
                 5
               </Typography>
             </IconButton>
-            <IconButton aria-label="favorites" sx={{ gap: 1 }} onClick={handleToggle}>
+            <IconButton
+              aria-label="favorites"
+              sx={{ gap: 1 }}
+              onClick={handleToggle}
+            >
               <ForumIcon sx={{ mt: 1, width: 18, height: 18 }} />
               <Typography variant="body2" sx={{ mt: 1 }}>
                 5
@@ -103,15 +120,42 @@ setToggle(!toggle)
               </Typography>
             </IconButton>
           </Box>
-          <Button size="small" variant="contained" onClick={handleClick}>
-            Back
-          </Button>
+          <Box sx={{ display: "flex", justifyContent: "space-evenly" }}>
+            <div>
+              <Button
+                size="small"
+                variant="contained"
+                color="success"
+                onClick={handleOpen}
+              >
+                update
+              </Button>
+              <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+              >
+                <Box sx={style}>
+                  <UpdateModal />
+                </Box>
+              </Modal>
+            </div>
+
+            <Button
+              size="small"
+              variant="contained"
+              color="error"
+              onClick={handleClick}
+            >
+              Delete
+            </Button>
+          </Box>
         </Box>
-        
-        <Box sx={toggle ? {display:"block"} : {display:"none"}}>
-        <CommentForm />
+
+        <Box sx={toggle ? { display: "block" } : { display: "none" }}>
+          <CommentForm />
         </Box>
-        
       </Card>
     </Box>
   );
---
+++ src/components/blog/UpdateModal.jsx
@@ -0,0 +1,112 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { TextField } from "@mui/material";
+
+// const bull = (
+//   <Box
+//     component="span"
+//     sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+//   >
+//     •
+//   </Box>
+// );
+
+const UpdateModal = () => {
+  return (
+    <Card
+      sx={{
+        backgroundColor: "#e2f5e2ec",
+        maxWidth: 350,
+        margin: "50px auto",
+        boxShadow: "0px 8px 16px rgba(0, 0, 0, 0.4)",
+      }}
+    >
+      <CardContent>
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+          <Typography variant="h4">Update Blog</Typography>
+          <TextField
+            id="title"
+            label="Title"
+            variant="standard"
+            sx={{ m: 1, minWidth: 120 }}
+          />
+          <TextField
+            id="url"
+            label="Image URL"
+            variant="standard"
+            sx={{ m: 1, minWidth: 120 }}
+          />
+
+          <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+            <InputLabel id="demo-simple-select-standard-label">
+              Category
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-standard-label"
+              id="demo-simple-select-standard"
+              // value={age}
+              // onChange={handleChange}
+              label="Category"
+              // sx={centered}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+            <InputLabel id="demo-simple-select-standard-label">
+              Status
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-standard-label"
+              id="demo-simple-select-standard"
+              // value={age}
+              // onChange={handleChange}
+              label="Category"
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+
+          <TextField
+            id="standard-multiline-static"
+            label="Content"
+            multiline
+            rows={4}
+            // defaultValue="Default Value"
+            variant="standard"
+            sx={{ m: 1, minWidth: 120 }}
+          />
+
+          <Button
+            variant="contained"
+            size="large"
+            color="success"
+            sx={{ m: 1, minWidth: 120 }}
+          >
+            Update Blog
+          </Button>
+        </Box>
+      </CardContent>
+    </Card>
+  );
+};
+export default UpdateModal;
