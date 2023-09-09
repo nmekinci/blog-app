2b8ac29d60eae206986bb118ab1dfa8a49fd769d
Must <nm.ekinci@gmail.com>
2023-09-09 11:40:06 +0200

    Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import { CardMedia, Typography } from "@mui/material";
 
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
-
-export default Profile
+    <Box sx={{ display:"flex", justifyContent:"center",mt:4 }}>
+      <Box
+        sx={{
+          width: 600,
+          height: "100%",
+          boxShadow: "0px 8px 16px rgba(0, 0, 0, 0.4)",
+        }}
+      >
+        <CardMedia
+          sx={{ margin:2 , height: 200 }}
+          image="https://images.ctfassets.net/hrltx12pl8hq/3j5RylRv1ZdswxcBaMi0y7/b84fa97296bd2350db6ea194c0dce7db/Music_Icon.jpg"
+          
+        />
+        <Typography
+            gutterBottom
+            variant="h5"
+            component="div"
+            sx={{ textAlign: "center" }}
+          >
+            User Name
+          </Typography><Typography
+            gutterBottom
+            variant="h5"
+            component="div"
+            sx={{ textAlign: "center" }}
+          >
+            Email adress
+          </Typography>
+      </Box>
+    </Box>
+  );
+};
+export default Profile;
