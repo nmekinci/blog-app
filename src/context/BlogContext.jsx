bd9ab3c849943f851f69122da97f978a030a1d02
Must <nm.ekinci@gmail.com>
2023-09-10 02:49:02 +0200

    getBlogs()

--- src/context/BlogContext.jsx
+++ src/context/BlogContext.jsx
@@ -11,14 +11,46 @@
 
 import React, { createContext, useReducer } from "react";
 import { initialBlogState, reducer } from "../reducer/blogReducer";
+import axios from "axios";
+
 
 export const BlogContext = createContext();
 
 const BlogContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialBlogState);
   const url = process.env.REACT_APP_BASE_URL;
+  const {
+    loading,
+    id,
+    title,
+    content,
+    image,
+    category,
+    publish_date,
+    author,
+    status,
+    slug,
+    comments,
+    category_name,
+    likes,
+    post_views,
+    comment_count,
+    likes_n,
+    error,
+  } = state
 
-  const values = {};
+  const getBlogs = async () => {
+    dispatch({type: "START"})
+    try {
+        const {data} = await axios(`${url}api/blogs/`)
+        dispatch({type:"SUCCESS"})
+    } catch (error) {
+        dispatch({type:"FAIL",payload: error})
+        console.log(error);
+    }
+  }
+
+  const values = {getBlogs};
   return <BlogContext.Provider value={values}>{children}</BlogContext.Provider>;
 };
 
